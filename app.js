e99046a89e7d30ecf45e44b60a3da3ec0af754cb
Vortiguen <ing.roshfort@gmail.com>
2022-05-18 11:08:31 -0500

    Se agrego un nuevo testo para la rama Text

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 console.console.log('Hello world');
 console.console.log('Este es mi primer mensaje en js')
 console.console.log('Ahora se guardara en la rama TEST')
+console.console.log('Este es un nuevo testo agregado para la rama test')
