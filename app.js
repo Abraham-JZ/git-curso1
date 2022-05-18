1104e15204999b379f66b07a624da9a536bc2513
Vortiguen <ing.roshfort@gmail.com>
2022-05-18 16:46:44 -0500

    Se agregaron dos lineas a index.html y app.js para poder usarse

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 console.console.log('Hello world');
 console.console.log('Este es mi primer mensaje en js')
 console.console.log('Ahora se guardara en la rama TEST')
-console.console.log('Este es un nuevo testo agregado para la rama test')
+console.console.log('Este es un nuevo texto agregado para la rama test')
+console.consoloe.log('Este es un nuevo texto para la rama de testeo')
